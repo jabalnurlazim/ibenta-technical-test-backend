0e56e1dcad4bfae47d707fe9bc4fd8a5ab919806
jablazim <jab@ckjproject.com>
2021-04-10 01:15:04 +0800

    Created basic User CRUD API

--- src/main/java/au/com/ibenta/springfox/SpringFoxConfiguration.java
+++ src/main/java/au/com/ibenta/springfox/SpringFoxConfiguration.java
@@ -84,8 +84,4 @@ public class SpringFoxConfiguration {
                 Collections.emptyList());
     }
 
-    @Bean
-    public ModelMapper modelMapper() {
-        return new ModelMapper();
-    }
 }
